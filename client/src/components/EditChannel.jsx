0cc72a4c73bf241b1c60b847ac33c73424e775ac
andrew001347 <84065006+andrew001347@users.noreply.github.com>
2023-03-13 23:36:40 -0700

    made the leave button
    the delete button does not work. it need to connect with the stream api and from there it is deleted

--- client/src/components/EditChannel.jsx
+++ client/src/components/EditChannel.jsx
@@ -1,61 +1,74 @@
-import React, { useState } from 'react'
-import { useChatContext } from 'stream-chat-react'
+import React, { useState } from 'react';
+import { useChatContext } from 'stream-chat-react';
 
-import { UserList } from './'
-import { CloseCreateChannel } from '../assets'
+import { UserList } from './';
+import { CloseCreateChannel } from '../assets';
 
 const ChannelNameInput = ({ channelName = '', setChannelName }) => {
-    const handleChange = (event) => {
-        event.preventDefault()
+  const handleChange = (event) => {
+    event.preventDefault();
 
-        setChannelName(event.target.value)
-    }
+    setChannelName(event.target.value);
+  };
 
-    return (
-        <div className='channel-name-input__wrapper'>
-            <p>Name</p>
-            <input value={channelName} onChange={handleChange} placeholder='channel-name' />
-            <p>Add Members</p>
-        </div>
-    )
-}
+  return (
+    <div className='channel-name-input__wrapper'>
+      <p>Name</p>
+      <input value={channelName} onChange={handleChange} placeholder='channel-name' />
+      <p>Add Members</p>
+    </div>
+  );
+};
 
 const EditChannel = ({ setIsEditing }) => {
-    const { channel } = useChatContext()
-    const [channelName, setChannelName] = useState(channel?.data?.name)
-    const [selectedUsers, setSelectedUsers] = useState([])
+  const { channel, client } = useChatContext();
+  const [channelName, setChannelName] = useState(channel?.data?.name);
+  const [selectedUsers, setSelectedUsers] = useState([]);
 
-    const updateChannel = async (event) => {
-        event.preventDefault()
+  const updateChannel = async (event) => {
+    event.preventDefault();
 
-        const nameChanged = channelName !== (channel.data.name || channel.data.id)
+    const nameChanged = channelName !== (channel.data.name || channel.data.id);
 
-        if (nameChanged) {
-            await channel.update({ name: channelName }, { text: `Channel name changed to ${channelName}`})
-        }
-
-        if (selectedUsers.length) {
-            await channel.addMembers(selectedUsers)
-        }
-
-        setChannelName(null)
-        setIsEditing(false)
-        setSelectedUsers([])
+    if (nameChanged) {
+      await channel.update({ name: channelName }, { text: `Channel name changed to ${channelName}` });
     }
 
-    return (
-        <div className='edit-channel_container'>
-            <div className='edit-channel__header'>
-                <p>Edit Channel</p>
-                <CloseCreateChannel setIsEditing={setIsEditing} />
-            </div>
-            <ChannelNameInput channelName={channelName} setChannelName={setChannelName} />
-            <UserList setSelectedUsers={setSelectedUsers} />
-            <div className='edit-channel__button-wrapper' onClick={updateChannel}>
-                <p>Save Changes</p>
-            </div>
-        </div>
-    )
-}
+    if (selectedUsers.length) {
+      await channel.addMembers(selectedUsers);
+    }
 
-export default EditChannel
+    setChannelName(null);
+    setIsEditing(false);
+    setSelectedUsers([]);
+  };
+
+  const leaveChannel = async () => {
+    const confirmed = window.confirm('Are you sure you want to leave/delete this channel?');
+    if (!confirmed) return;
+
+    await client.channel(channel.type, channel.id).removeMembers([client.userID]);
+
+    setIsEditing(false);
+    // Navigate to a different channel or page
+  };
+
+  return (
+    <div className='edit-channel_container'>
+      <div className='edit-channel__header'>
+        <p>Edit Channel</p>
+        <CloseCreateChannel setIsEditing={setIsEditing} />
+      </div>
+      <ChannelNameInput channelName={channelName} setChannelName={setChannelName} />
+      <UserList setSelectedUsers={setSelectedUsers} />
+      <div className='edit-channel__button-wrapper' onClick={updateChannel}>
+        <p>Save Changes</p>
+      </div>
+      <div className='edit-channel__button-wrapper' onClick={leaveChannel}>
+        <p>Leave/Delete Channel</p>
+      </div>
+    </div>
+  );
+};
+
+export default EditChannel;

937224ab6d540660c8c83114e3032355adfddfa0
jredmolina <jared.a.molina01@gmail.com>
2023-03-19 14:52:42 -0700

    Changed colors of calendar icon and added new log out icon

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,140 +1,156 @@
-import React, { useState, useEffect } from 'react'
-import { useChatContext } from 'stream-chat-react'
-import { CalendarHeader } from './CalendarHeader'
-import { Day } from './Day'
-import { NewEventModal } from './NewEventModal'
-import { useDate } from '../hooks/useDate'
+import React, { useState, useEffect } from "react";
+import { useChatContext } from "stream-chat-react";
+import { CalendarHeader } from "./CalendarHeader";
+import { Day } from "./Day";
+import { NewEventModal } from "./NewEventModal";
+import { useDate } from "../hooks/useDate";
 
 const Calendar = (user) => {
-    const [nav, setNav] = useState(0)
-    const [clicked, setClicked] = useState()
-    const [events, setEvents] = useState([])
-    const [eventFlag, setEventFlag] = useState(false)
-    const [currentColor, setCurrentColor] = useState('#58bae4')
-    const { channel } = useChatContext()
-    const members = Object.values(channel?.state?.members || {})
-    const usernames = members.map(({ user }) => user.name).filter(user => user !== 'Personal')
-    if (usernames.length === 0) {
-        usernames.push(user.data.username)
-    }
+  const [nav, setNav] = useState(0);
+  const [clicked, setClicked] = useState();
+  const [events, setEvents] = useState([]);
+  const [eventFlag, setEventFlag] = useState(false);
+  const [currentColor, setCurrentColor] = useState("#58bae4");
+  const { channel } = useChatContext();
+  const members = Object.values(channel?.state?.members || {});
+  const usernames = members
+    .map(({ user }) => user.name)
+    .filter((user) => user !== "Personal");
+  if (usernames.length === 0) {
+    usernames.push(user.data.username);
+  }
 
-    useEffect(() => {
-        if (channel) {
-            getEvents()
-        }
-    }, [channel])
-    
-    async function getEvents() {
-        fetch('http://localhost:5000/events')
-            .then(response => response.json())
-            .then(data => {
-                setEvents(data
-                    .filter(event => 
-                        event.attendees.some(attendeeName => usernames.includes(attendeeName))
-                    )
-                    .filter(event =>
-                        event.attendees.some(attendeeName => attendeeName === user.data.username)
-                        ^ (event.attendees.every(attendeeName => attendeeName !== user.data.username) && !event._private)
-                    )
-                )
-                console.log(data)
-            })
-            .catch(error => console.error(error));
+  useEffect(() => {
+    if (channel) {
+      getEvents();
     }
+  }, [channel]);
 
-    async function createEvent(event) {
-        fetch('http://localhost:5000/events', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(event)
-          })
-            .then(response => {
-                response.json()
-            })
-            .then(data => { 
-                console.log('Created event:', event)
-                getEvents();
-             })
-            .catch(error => console.error(error))
-    }
+  async function getEvents() {
+    fetch("http://localhost:5000/events")
+      .then((response) => response.json())
+      .then((data) => {
+        setEvents(
+          data
+            .filter((event) =>
+              event.attendees.some((attendeeName) =>
+                usernames.includes(attendeeName)
+              )
+            )
+            .filter(
+              (event) =>
+                event.attendees.some(
+                  (attendeeName) => attendeeName === user.data.username
+                ) ^
+                (event.attendees.every(
+                  (attendeeName) => attendeeName !== user.data.username
+                ) && !event._private)
+            )
+        );
+        console.log(data);
+      })
+      .catch((error) => console.error(error));
+  }
 
-    const { days, dateDisplay } = useDate(events, nav)
-    
-    return (
+  async function createEvent(event) {
+    fetch("http://localhost:5000/events", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(event),
+    })
+      .then((response) => {
+        response.json();
+      })
+      .then((data) => {
+        console.log("Created event:", event);
+        getEvents();
+      })
+      .catch((error) => console.error(error));
+  }
+
+  const { days, dateDisplay } = useDate(events, nav);
+
+  return (
     <>
-        <div id="calendar-body">
-            <div id="calendar-container">
-                <CalendarHeader 
-                    dateDisplay={dateDisplay}
-                    onNext={() => setNav(nav + 1)}
-                    onBack={() => setNav(nav - 1)}
-                />
+      <div id="calendar-body">
+        <div id="calendar-container">
+          <CalendarHeader
+            dateDisplay={dateDisplay}
+            onNext={() => setNav(nav + 1)}
+            onBack={() => setNav(nav - 1)}
+          />
 
-                <div id="weekdays">
-                    <div>Sunday</div>
-                    <div>Monday</div>
-                    <div>Tuesday</div>
-                    <div>Wednesday</div>
-                    <div>Thursday</div>
-                    <div>Friday</div>
-                    <div>Saturday</div>
-                </div>
+          <div id="weekdays">
+            <div>Sunday</div>
+            <div>Monday</div>
+            <div>Tuesday</div>
+            <div>Wednesday</div>
+            <div>Thursday</div>
+            <div>Friday</div>
+            <div>Saturday</div>
+          </div>
 
-                <div id="calendar">
-                    {days.map((d, index) => (
-                        <Day 
-                            key={index}
-                            day={d}
-                            events={events}
-                            currentColor={currentColor}
-                            setCurrentColor={setCurrentColor}
-                            onClick={() => {
-                                if (d.value !== 'padding') {
-                                    setClicked(d.date)
-                                    //console.log(user.data.username)
-                                    console.log(usernames)
-                                    if (eventFlag === true) {
-                                        setClicked(null)
-                                    }
-                                }
-                            }}
-                            eventFlag={eventFlag}
-                            setEventFlag={setEventFlag}
-                            getEvents={getEvents}
-                        />
-                    ))}
-                </div>
-            </div>
-
-            {
-                eventFlag === false && clicked &&
-                <NewEventModal
-                    onClose={() => {
-                        setClicked(null)
-                    }}
-                    onSave={(eventTitleInput, startTimeInput, endTimeInput, locationInput, privateInput, colorInput) => {
-                        const event = {
-                            date: clicked,
-                            title: eventTitleInput,
-                            startTime: startTimeInput,
-                            endTime: endTimeInput,
-                            location: locationInput,
-                            _private: privateInput,
-                            color: colorInput,
-                            attendees: usernames
-                        }
-                        createEvent(event)
-                        setClicked(null)
-                    }}
-                    currentColor={currentColor}
-                    setCurrentColor={setCurrentColor}
-                />
-            }
+          <div id="calendar">
+            {days.map((d, index) => (
+              <Day
+                key={index}
+                day={d}
+                events={events}
+                currentColor={currentColor}
+                setCurrentColor={setCurrentColor}
+                onClick={() => {
+                  if (d.value !== "padding") {
+                    setClicked(d.date);
+                    //console.log(user.data.username)
+                    console.log(usernames);
+                    if (eventFlag === true) {
+                      setClicked(null);
+                    }
+                  }
+                }}
+                eventFlag={eventFlag}
+                setEventFlag={setEventFlag}
+                getEvents={getEvents}
+              />
+            ))}
+          </div>
         </div>
-    </>
-  )
-}
 
-export default Calendar
+        {eventFlag === false && clicked && (
+          <NewEventModal
+            onClose={() => {
+              setClicked(null);
+            }}
+            onSave={(
+              eventTitleInput,
+              startTimeInput,
+              endTimeInput,
+              locationInput,
+              privateInput,
+              colorInput
+            ) => {
+              const event = {
+                date: clicked,
+                title: eventTitleInput,
+                startTime: startTimeInput,
+                endTime: endTimeInput,
+                location: locationInput,
+                _private: privateInput,
+                color: colorInput,
+                attendees: usernames,
+              };
+              createEvent(event);
+              setClicked(null);
+            }}
+            currentColor={currentColor}
+            setCurrentColor={setCurrentColor}
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Calendar;
--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,198 +1,227 @@
-import React, { useState } from 'react'
-import { ChannelList, useChatContext } from 'stream-chat-react'
-import Cookies from 'universal-cookie'
+import React, { useState } from "react";
+import { ChannelList, useChatContext } from "stream-chat-react";
+import Cookies from "universal-cookie";
 
-import { ChannelSearch, PersonalChannel, TeamChannelList, TeamChannelPreview } from './'
-import CalendarIcon from '../assets/calendar.png'
-import LogoutIcon from '../assets/logout.png'
+import {
+  ChannelSearch,
+  PersonalChannel,
+  TeamChannelList,
+  TeamChannelPreview,
+} from "./";
+import CalendarIcon from "../assets/calendar.png";
+import LogoutIcon from "../assets/logout.png";
 
-const cookies = new Cookies()
+const cookies = new Cookies();
 
 const SideBar = ({ logout }) => (
-    <div className='channel-list__sidebar'>
-        <div className='channel-list__sidebar__icon1'>
-            <div className='icon1__inner'>
-                <img src={CalendarIcon} alt='Calendar' width='30' />
-            </div>
-        </div>
-        <div className='channel-list__sidebar__icon2'>
-            <div className='icon1__inner' onClick={logout}>
-                <img src={LogoutIcon} alt='Logout'  title= 'Sign Out' width='30' />
-            </div>
-        </div>
+  <div className="channel-list__sidebar">
+    <div className="channel-list__sidebar__icon1">
+      <div className="icon1__inner">
+        <img src={CalendarIcon} alt="Calendar" width="30" />
+      </div>
     </div>
-)
+    <div className="channel-list__sidebar__icon2">
+      <div className="icon1__inner" onClick={logout}>
+        <img src={LogoutIcon} alt="Logout" title="Sign Out" width="40" />
+      </div>
+    </div>
+  </div>
+);
 
 const CompanyHeader = () => (
-    <div className='channel-list__header'>
-        <p className='channel-list__header__text'>SyncSchedule</p>
-    </div>
-)
+  <div className="channel-list__header">
+    <p className="channel-list__header__text">SyncSchedule</p>
+  </div>
+);
 
 const customPersonalChannelFilter = (channels) => {
-    return channels.filter((channel) => {
-      if (channel.type !== 'messaging') {
-        return false;
-      }
-      const members = Object.values(channel.state.members || {});
-      return members.some((member) => member.user.id === '18c0d74c3413610d5f814f9964668116');
-    });
-  };
+  return channels.filter((channel) => {
+    if (channel.type !== "messaging") {
+      return false;
+    }
+    const members = Object.values(channel.state.members || {});
+    return members.some(
+      (member) => member.user.id === "18c0d74c3413610d5f814f9964668116"
+    );
+  });
+};
 
 const customChannelTeamFilter = (channels) => {
-    return channels.filter((channel) => channel.type === 'team')
-}
+  return channels.filter((channel) => channel.type === "team");
+};
 
 const customChannelMessagingFilter = (channels) => {
-    return channels.filter((channel) => {
-        if (channel.type !== 'messaging') {
-          return false;
-        }
-        const members = Object.values(channel.state.members || {});
-        return !members.some((member) => member.user.id === '18c0d74c3413610d5f814f9964668116');
+  return channels.filter((channel) => {
+    if (channel.type !== "messaging") {
+      return false;
+    }
+    const members = Object.values(channel.state.members || {});
+    return !members.some(
+      (member) => member.user.id === "18c0d74c3413610d5f814f9964668116"
+    );
+  });
+};
+
+const ChannelListContent = ({
+  isCreating,
+  setIsCreating,
+  setCreateType,
+  setIsEditing,
+  isSignup,
+  setToggleContainer,
+  data,
+}) => {
+  const { client } = useChatContext();
+
+  const logout = () => {
+    cookies.remove("token");
+    cookies.remove("userId");
+    cookies.remove("username");
+    cookies.remove("fullName");
+    cookies.remove("avatarURL");
+    cookies.remove("hashedPassword");
+    cookies.remove("phoneNumber");
+
+    window.location.reload();
+  };
+
+  const createMessagingChannel = async () => {
+    try {
+      const channel = client.channel("messaging", {
+        members: [client.userID, "18c0d74c3413610d5f814f9964668116"],
       });
-}
-
-const ChannelListContent = ({ isCreating, setIsCreating, setCreateType, setIsEditing, isSignup, setToggleContainer, data }) => {
-    const { client } = useChatContext()
-
-    const logout = () => {
-        cookies.remove('token');
-        cookies.remove('userId');
-        cookies.remove('username');
-        cookies.remove('fullName');
-        cookies.remove('avatarURL');
-        cookies.remove('hashedPassword');
-        cookies.remove('phoneNumber');
-
-        window.location.reload()
+      await channel.create();
+    } catch (error) {
+      console.error("Error creating personal channel:", error);
     }
+  };
 
-    const createMessagingChannel = async () => {
-        try {
-          const channel = client.channel('messaging', { members: [client.userID, '18c0d74c3413610d5f814f9964668116'] })
-          await channel.create()
-        } catch (error) {
-          console.error('Error creating personal channel:', error)
-        }
-    }
+  if (isSignup && data.username !== "Personal") {
+    createMessagingChannel();
+  }
 
-    if (isSignup && data.username !== 'Personal') {
-        createMessagingChannel()
-    }
+  const filters = { members: { $in: [client.userID] } };
 
-    const filters = { members: { $in: [client.userID] } }
+  return (
+    <>
+      <SideBar logout={logout} />
+      <div className="channel-list__list__wrapper">
+        <CompanyHeader />
+        <ChannelSearch setToggleContainer={setToggleContainer} />
+        <ChannelList
+          filters={filters}
+          channelRenderFilterFn={customPersonalChannelFilter}
+          List={(listProps) => (
+            <PersonalChannel {...listProps} type="messaging" />
+          )}
+          Preview={(previewProps) => (
+            <TeamChannelPreview
+              {...previewProps}
+              setIsCreating={setIsCreating}
+              setIsEditing={setIsEditing}
+              setToggleContainer={setToggleContainer}
+              type="messaging"
+            />
+          )}
+        />
+        <ChannelList
+          filters={filters}
+          channelRenderFilterFn={customChannelTeamFilter}
+          List={(listProps) => (
+            <TeamChannelList
+              {...listProps}
+              type="team"
+              isCreating={isCreating}
+              setIsCreating={setIsCreating}
+              setCreateType={setCreateType}
+              setIsEditing={setIsEditing}
+              setToggleContainer={setToggleContainer}
+            />
+          )}
+          Preview={(previewProps) => (
+            <TeamChannelPreview
+              {...previewProps}
+              setIsCreating={setIsCreating}
+              setIsEditing={setIsEditing}
+              setToggleContainer={setToggleContainer}
+              type="team"
+            />
+          )}
+        />
+        <ChannelList
+          filters={filters}
+          channelRenderFilterFn={customChannelMessagingFilter}
+          List={(listProps) => (
+            <TeamChannelList
+              {...listProps}
+              type="messaging"
+              isCreating={isCreating}
+              setIsCreating={setIsCreating}
+              setCreateType={setCreateType}
+              setIsEditing={setIsEditing}
+              setToggleContainer={setToggleContainer}
+            />
+          )}
+          Preview={(previewProps) => (
+            <TeamChannelPreview
+              {...previewProps}
+              setIsCreating={setIsCreating}
+              setIsEditing={setIsEditing}
+              setToggleContainer={setToggleContainer}
+              type="messaging"
+            />
+          )}
+        />
+      </div>
+    </>
+  );
+};
 
-    return (
-        <>
-            <SideBar logout={logout} />
-            <div className='channel-list__list__wrapper'>
-                <CompanyHeader />
-                <ChannelSearch setToggleContainer={setToggleContainer}/>
-                <ChannelList
-                    filters={filters}
-                    channelRenderFilterFn={customPersonalChannelFilter}
-                    List={(listProps) => (
-                        <PersonalChannel 
-                            {...listProps}
-                            type='messaging'
-                        />
-                    )}
-                    Preview={(previewProps) => (
-                        <TeamChannelPreview 
-                            {...previewProps}
-                            setIsCreating={setIsCreating}
-                            setIsEditing={setIsEditing}
-                            setToggleContainer={setToggleContainer}
-                            type='messaging'
-                        />
-                    )}
-                />
-                <ChannelList
-                    filters={filters}
-                    channelRenderFilterFn={customChannelTeamFilter}
-                    List={(listProps) => (
-                        <TeamChannelList 
-                            {...listProps}
-                            type='team'
-                            isCreating={isCreating}
-                            setIsCreating={setIsCreating}
-                            setCreateType={setCreateType}
-                            setIsEditing={setIsEditing}
-                            setToggleContainer={setToggleContainer}
-                        />
-                    )}
-                    Preview={(previewProps) => (
-                        <TeamChannelPreview 
-                            {...previewProps}
-                            setIsCreating={setIsCreating}
-                            setIsEditing={setIsEditing}
-                            setToggleContainer={setToggleContainer}
-                            type='team'
-                        />
-                    )}
-                />
-                <ChannelList
-                    filters={filters}
-                    channelRenderFilterFn={customChannelMessagingFilter}
-                    List={(listProps) => (
-                        <TeamChannelList 
-                            {...listProps}
-                            type='messaging'
-                            isCreating={isCreating}
-                            setIsCreating={setIsCreating}
-                            setCreateType={setCreateType}
-                            setIsEditing={setIsEditing}
-                            setToggleContainer={setToggleContainer}
-                        />
-                    )}
-                    Preview={(previewProps) => (
-                        <TeamChannelPreview 
-                            {...previewProps}
-                            setIsCreating={setIsCreating}
-                            setIsEditing={setIsEditing}
-                            setToggleContainer={setToggleContainer}
-                            type='messaging'
-                        />
-                    )}
-                />
-            </div>
-        </>
-    )
-}
+const ChannelListContainer = ({
+  setCreateType,
+  setIsCreating,
+  setIsEditing,
+  isSignup,
+  data,
+}) => {
+  const [toggleContainer, setToggleContainer] = useState(false);
 
-const ChannelListContainer = ({ setCreateType, setIsCreating, setIsEditing, isSignup, data}) => {
-    const [toggleContainer, setToggleContainer] = useState(false)
+  return (
+    <>
+      <div className="channel-list__container">
+        <ChannelListContent
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
+          isSignup={isSignup}
+          data={data}
+        />
+      </div>
 
-    return (
-        <>
-            <div className='channel-list__container'>
-                <ChannelListContent 
-                    setIsCreating={setIsCreating}
-                    setCreateType={setCreateType}
-                    setIsEditing={setIsEditing}
-                    isSignup={isSignup}
-                    data={data}
-                />
-            </div>
+      <div
+        className="channel-list__container-responsive"
+        style={{
+          left: toggleContainer ? "0%" : "-89%",
+          backgroundColor: "#005fff",
+        }}
+      >
+        <div
+          className="channel-list__container-toggle"
+          onClick={() =>
+            setToggleContainer((prevToggleContainer) => !prevToggleContainer)
+          }
+        ></div>
+        <ChannelListContent
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
+          isSignup={isSignup}
+          data={data}
+          setToggleContainer={setToggleContainer}
+        />
+      </div>
+    </>
+  );
+};
 
-            <div className='channel-list__container-responsive'
-                style={{ left: toggleContainer ? '0%' : '-89%', backgroundColor: '#005fff'}}
-            >
-                <div className='channel-list__container-toggle' onClick={() => setToggleContainer((prevToggleContainer) => !prevToggleContainer)}>
-                </div>
-                <ChannelListContent 
-                    setIsCreating={setIsCreating}
-                    setCreateType={setCreateType}
-                    setIsEditing={setIsEditing}
-                    isSignup={isSignup}
-                    data={data}
-                    setToggleContainer={setToggleContainer}
-                />
-            </div>
-        </>
-    )
-
-}
-
-export default ChannelListContainer
+export default ChannelListContainer;

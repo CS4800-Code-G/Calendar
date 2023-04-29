5a743158654cd443c264dafdf758dbc64d316f14
jredmolina <jared.a.molina01@gmail.com>
2023-04-29 04:47:34 -0700

    Changed origin

--- server/index.js
+++ server/index.js
@@ -1,137 +1,140 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const path = require('path')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 
-const authRoutes = require('./routes/auth.js')
+const authRoutes = require("./routes/auth.js");
 
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 
-const mongoClient = new MongoClient(process.env.DATABASE_URL, { useNewUrlParser: true });
+const mongoClient = new MongoClient(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+});
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-require('dotenv').config()
+require("dotenv").config();
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID
-const authToken = process.env.TWILIO_AUTH_TOKEN
-const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID
-const twilioClient = require('twilio')(accountSid, authToken)
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+const twilioClient = require("twilio")(accountSid, authToken);
 const corsOptions = {
-    origin: ['http://localhost:3000', 'https://example.com'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  };
+  origin: ["http://localhost:8080", "https://example.com"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
 app.use(cors());
 
-app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.json());
+app.use(express.urlencoded());
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
 
-const eventsRouter = require('./routes/events')
-app.use('/events', eventsRouter)
+const eventsRouter = require("./routes/events");
+app.use("/events", eventsRouter);
 
-const usersRouter = require('./routes/users')
-app.use('/users', usersRouter)
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
 
-const channelsRouter = require('./routes/channels')
-app.use('/channels', channelsRouter)
+const channelsRouter = require("./routes/channels");
+app.use("/channels", channelsRouter);
 
-const _dirname = path.dirname('')
-const buildPath = path.join(_dirname, '../client/build')
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../client/build");
 
-app.use(express.static(buildPath))
+app.use(express.static(buildPath));
 
-app.get('/', function(req, res) {
-    res.sendFile(
-        path.join(__dirname, '../client/build/index.html'),
-        function (err) {
-            if (err) {
-                res.status(500).send(err)
-            }
-        }
-    )
-})
-
-app.get('/stream-events', async (req, res) => {
-    const db = await mongoClient.connect()
-    const collection = db.db('Calendar').collection('events')
-  
-    const changeStream = collection.watch()
-    changeStream.on('change', () => {
-      // Send the updated data to the client
-      res.write('event: update\ndata: updated\n\n')
-    })
-  
-    res.set({
-      'Content-Type': 'text/event-stream',
-      'Cache-Control': 'no-cache',
-      'Connection': 'keep-alive'
-    })
-})
-
-app.get('/stream-users', async (req, res) => {
-    const db = await mongoClient.connect()
-    const collection = db.db('Calendar').collection('users')
-  
-    const changeStream = collection.watch()
-    changeStream.on('change', () => {
-      // Send the updated data to the client
-      res.write('event: update\ndata: updated\n\n')
-    })
-  
-    res.set({
-      'Content-Type': 'text/event-stream',
-      'Cache-Control': 'no-cache',
-      'Connection': 'keep-alive'
-    })
-})
-
-app.get('/stream-channels', async (req, res) => {
-    const db = await mongoClient.connect()
-    const collection = db.db('Calendar').collection('channels')
-  
-    const changeStream = collection.watch()
-    changeStream.on('change', () => {
-      // Send the updated data to the client
-      res.write('event: update\ndata: updated\n\n')
-    })
-  
-    res.set({
-      'Content-Type': 'text/event-stream',
-      'Cache-Control': 'no-cache',
-      'Connection': 'keep-alive'
-    })
-})
-
-app.post('/', (req, res) => {
-    const { message, user: sender, type, members } = req.body
-
-    if (type === 'message.new') {
-        members
-            .filter((member) => member.user_id !== sender.id)
-            .forEach(({ user }) => {
-                if (!user.online) {
-                    twilioClient.messages.create({
-                        body: `You have a new message from ${message.user.fullName} - ${message.text}`,
-                        messagingServiceSid: messagingServiceSid,
-                        to: `+1${user.phoneNumber}`
-                    })
-                        .then(() => console.log('Message sent!'))
-                        .catch(err => console.log(err))
-                }
-            })
-
-            return res.status(200).send('Message sent!')
+app.get("/", function (req, res) {
+  res.sendFile(
+    path.join(__dirname, "../client/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
     }
+  );
+});
 
-    return res.status(200).send('Not a new message request')
-})
+app.get("/stream-events", async (req, res) => {
+  const db = await mongoClient.connect();
+  const collection = db.db("Calendar").collection("events");
 
-app.use('/auth', authRoutes)
+  const changeStream = collection.watch();
+  changeStream.on("change", () => {
+    // Send the updated data to the client
+    res.write("event: update\ndata: updated\n\n");
+  });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  res.set({
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+  });
+});
+
+app.get("/stream-users", async (req, res) => {
+  const db = await mongoClient.connect();
+  const collection = db.db("Calendar").collection("users");
+
+  const changeStream = collection.watch();
+  changeStream.on("change", () => {
+    // Send the updated data to the client
+    res.write("event: update\ndata: updated\n\n");
+  });
+
+  res.set({
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+  });
+});
+
+app.get("/stream-channels", async (req, res) => {
+  const db = await mongoClient.connect();
+  const collection = db.db("Calendar").collection("channels");
+
+  const changeStream = collection.watch();
+  changeStream.on("change", () => {
+    // Send the updated data to the client
+    res.write("event: update\ndata: updated\n\n");
+  });
+
+  res.set({
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+  });
+});
+
+app.post("/", (req, res) => {
+  const { message, user: sender, type, members } = req.body;
+
+  if (type === "message.new") {
+    members
+      .filter((member) => member.user_id !== sender.id)
+      .forEach(({ user }) => {
+        if (!user.online) {
+          twilioClient.messages
+            .create({
+              body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+              messagingServiceSid: messagingServiceSid,
+              to: `+1${user.phoneNumber}`,
+            })
+            .then(() => console.log("Message sent!"))
+            .catch((err) => console.log(err));
+        }
+      });
+
+    return res.status(200).send("Message sent!");
+  }
+
+  return res.status(200).send("Not a new message request");
+});
+
+app.use("/auth", authRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
